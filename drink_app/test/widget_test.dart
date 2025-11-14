228fcda3e9bed96da2c9e6f2f0bd2fe4e4a58bb4
Dzaha-code <mdzakwanhf@gmail.com>
2025-11-14 19:22:00 +0700

    Update fitur reminder dan perbaikan UI

--- drink_app/test/widget_test.dart
+++ drink_app/test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:drink_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Menambah air +200 ml', (WidgetTester tester) async {
+    // Build aplikasi dan trigger frame
+    await tester.pumpWidget(const DrinkApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verifikasi bahwa nilai awal adalah 0 ml
+    expect(find.text('Saat ini: 0 ml'), findsOneWidget);
+    expect(find.text('Saat ini: 200 ml'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap tombol "+200 ml"
+    await tester.tap(find.text('+200 ml'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verifikasi bahwa nilai sekarang menjadi 200 ml
+    expect(find.text('Saat ini: 0 ml'), findsNothing);
+    expect(find.text('Saat ini: 200 ml'), findsOneWidget);
   });
 }
